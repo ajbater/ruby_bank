12c9a7920f93b2bc0b246448bd954d6450d9839b
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-27 11:36:02 +0000

    account has a balance which is 0 by default

---
+++ spec/account_spec.rb
@@ -0,0 +1,9 @@
+require 'account'
+
+describe Account do
+  subject(:account) { described_class.new }
+
+  it 'should have a balance which is 0 by default' do
+    expect(account.balance).to eq 0
+  end
+end
