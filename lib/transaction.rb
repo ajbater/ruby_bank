74bc5c96d3decf764611c6060702afb14314ec54
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-27 15:13:51 +0000

    transaction has a time

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,11 @@
 class Transaction
 
-  attr_reader :type, :amount
+  attr_reader :type, :amount, :date
 
   def initialize(type, amount)
     @type = type
     @amount = amount
+    @date = Time.new.strftime("%d.%m.%Y")
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,6 +3,7 @@ require 'transaction'
 describe Transaction do
   let(:deposit) { double :deposit }
   let(:amount) { double :amount }
+  let(:time) { double :time }
   subject(:transaction) { described_class.new(deposit, amount) }
 
   it 'has a type' do
@@ -11,5 +12,11 @@ describe Transaction do
 
   it 'has an amount' do
     expect(transaction.amount).to eq amount
+  end
+
+  it 'has a date' do
+    allow(Time).to receive(:new).and_return(time)
+    allow(time).to receive(:strftime).and_return('26.02.2017')
+    expect(transaction.date).to eq '26.02.2017'
   end
 end
